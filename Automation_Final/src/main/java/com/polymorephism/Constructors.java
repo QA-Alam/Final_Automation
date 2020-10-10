bffde483a726d3080864d3406ee2011a62a7f578
QA-Alam <alam.itprofessinal@gmailcom>
2020-10-10 18:18:53 -0400

    Commit by alam

---
+++ Automation_Final/src/main/java/com/polymorephism/Constructors.java
@@ -0,0 +1,15 @@
+package com.polymorephism;
+
+public class Constructors {
+
+	  // Java constructors overloading 
+	
+	
+	Constructors(String name){
+		
+	}
+	
+	Constructors(int age){
+		
+	}
+}
---
+++ Automation_Final/src/test/java/java/practice/code/TestClass.java
@@ -0,0 +1,12 @@
+package java.practice.code;
+
+public class TestClass {
+	
+public static void main(String[] args) {
+	
+System.out.println("A");
+	
+}
+	
+	
+}
